4609e72487fc0c9556eb71491f4e23c5dd05023b
Dylachee <saaakbaevv@gmail.com>
2023-05-13 11:33:21 +0600

    NKTK

--- NLTK.py
+++ NLTK.py
@@ -2,6 +2,7 @@ import nltk
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
 # Загрузка данных из CSV файла
 def load_data(filename):
@@ -31,14 +32,14 @@ def split_morphemes(word):
 
 # Загрузка тренировочных данных
 train_data = load_data('train_lr.csv')
-train_data = train_data[:1000]  # Ограничение до 1000 строк
+train_data = train_data[:200]  # Ограничение до 1000 строк
 
 # Загрузка тестовых данных
 test_data = load_data('test_lr.csv')
 
 # Извлечение слов и их меток из данных
-train_words = train_data['Word'].tolist()[:1000]  # Используйте только первые 1000 слов
-train_labels = train_data['Tag'].tolist()[:1000]  # Используйте только первые 1000 меток
+train_words = train_data['Word'].tolist()[:200]  # Используйте только первые 1000 слов
+train_labels = train_data['Tag'].tolist()[:200]  # Используйте только первые 1000 меток
 test_words = test_data['Word'].tolist()  # Получите все слова из тестовых данных
 
 # Токенизация слов
@@ -50,15 +51,34 @@ train_morphemes = [split_morphemes(word) for word in train_tokens]
 test_morphemes = [split_morphemes(word) for word in test_tokens]
 
 # Создание датафрейма с тренировочными данными
-train_df = pd.DataFrame({'Word': train_tokens, 'Morphemes': train_morphemes})
+train_df = pd.DataFrame({'Word': train_tokens[:200], 'Morphemes': train_morphemes[:200]})
 train_df['Root'] = train_df['Morphemes'].apply(lambda x: x[0])
 train_df['Affix'] = train_df['Morphemes'].apply(lambda x: x[1] if len(x) > 1 else '')
-train_df['Tag'] = train_labels[:1000]  # Используйте только первые 1000 меток
 
 # Создание датафрейма с тестовыми данными
 test_df = pd.DataFrame({'Word': test_tokens, 'Morphemes': test_morphemes})
 test_df['Root'] = test_df['Morphemes'].apply(lambda x: x[0])
 test_df['Affix'] = test_df['Morphemes'].apply(lambda x: x[1] if len(x) > 1 else '')
+
+# Создание экземпляра OneHotEncoder
+one_hot_encoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
+
+# Объединение тренировочного и тестового датасетов для одновременной обработки признаков
+combined_df = pd.concat([train_df, test_df])
+
+# One-Hot Encoding для столбцов 'Root' и 'Affix'
+one_hot_encoded = one_hot_encoder.fit_transform(combined_df[['Root', 'Affix']])
+
+# Разделение обратно на тренировочный и тестовый датасеты
+train_one_hot_encoded = one_hot_encoded[:len(train_df)]
+test_one_hot_encoded = one_hot_encoded[len(train_df):]
+
+# Замена столбцов 'Root' и 'Affix' в тренировочном и тестовом датасетах
+train_df.drop(['Root', 'Affix'], axis=1, inplace=True)
+train_df = pd.concat([train_df, pd.DataFrame(train_one_hot_encoded)], axis=1)
+
+test_df.drop(['Root', 'Affix'], axis=1, inplace=True)
+test_df = pd.concat([test_df, pd.DataFrame(test_one_hot_encoded)], axis=1)
 
 # Обработка и преобразование данных
 train_X = train_df[['Root', 'Affix']]
@@ -76,4 +96,3 @@ predictions = lr.predict(test_X)
 submission_file = 'my_submission.csv'
 test_df['Tag'] = predictions
 test_df[['Word', 'Root', 'Affix', 'Tag']].to_csv(submission_file, index=False, header=True)
-
