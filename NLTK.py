9014f550926be351d031c4bcbcc6565441f20c2e
Dylachee <saaakbaevv@gmail.com>
2023-05-13 13:18:35 +0600

    ex

--- NLTK.py
+++ NLTK.py
@@ -17,7 +17,16 @@ def split_morphemes(word):
         (['кыр', 'бол'], 'кыр', 'бол'),  # Пример: кыр + бол -> кыр - корень, бол - аффикс
         (['тур', 'ул'], 'тур', 'ул'),  # Пример: тур + ул -> тур - корень, ул - аффикс
         # Правило 2: Определение корней и аффиксов на основе заданных правил
-        # Здесь вы можете добавить свои правила разделения на морфемы
+        (['мо', 'ст'], 'мо', 'ст'),  # Пример: мо + ст -> мо - корень, ст - аффикс
+        (['быр', 'ул'], 'быр', 'ул'),  # Пример: быр + ул -> быр - корень, ул - аффикс
+        # Правило 3:
+        (['пред', 'ост'], 'пред', 'ост'),  # Пример: пред + ост -> пред - корень, ост - аффикс
+        (['пере', 'ход'], 'пере', 'ход'),  # Пример: пере + ход -> пере - корень, ход - аффикс
+        # Добавьте свои правила ниже
+        (['при', 'ход'], 'при', 'ход'),  # Пример: при + ход -> при - корень, ход - аффикс
+        (['раз', 'дел'], 'раз', 'дел'),  # Пример: раз + дел -> раз - корень, дел - аффикс
+        (['с', 'каз'], 'с', 'каз'),  # Пример: с + каз -> с - корень, каз - аффикс
+        (['по', 'ход'], 'по', 'ход'),  # Пример: по + ход -> по - корень, ход - аффикс
     ]
 
     # Проверка слова на соответствие каждому правилу
@@ -28,18 +37,19 @@ def split_morphemes(word):
             if root.endswith(suffix_root):
                 affix = root[:len(root) - len(suffix_root)]
                 return [root, affix]
-    return [word]  # Если нет совпадений, возвращаем слово как одну морфему
+    return [word]
+
 
 # Загрузка тренировочных данных
 train_data = load_data('train_lr.csv')
-train_data = train_data[:200]  # Ограничение до 1000 строк
+train_data = train_data[:10]  # Ограничение до 1000 строк
 
 # Загрузка тестовых данных
 test_data = load_data('test_lr.csv')
 
 # Извлечение слов и их меток из данных
-train_words = train_data['Word'].tolist()[:200]  # Используйте только первые 1000 слов
-train_labels = train_data['Tag'].tolist()[:200]  # Используйте только первые 1000 меток
+train_words = train_data['Word'].tolist()[:1000]  # Используйте только первые 1000 слов
+train_labels = train_data['Tag'].tolist()[:1000]  # Используйте только первые 1000 меток
 test_words = test_data['Word'].tolist()  # Получите все слова из тестовых данных
 
 # Токенизация слов
@@ -51,7 +61,7 @@ train_morphemes = [split_morphemes(word) for word in train_tokens]
 test_morphemes = [split_morphemes(word) for word in test_tokens]
 
 # Создание датафрейма с тренировочными данными
-train_df = pd.DataFrame({'Word': train_tokens[:200], 'Morphemes': train_morphemes[:200]})
+train_df = pd.DataFrame({'Word': train_tokens[:1000], 'Morphemes': train_morphemes[:1000]})
 train_df['Root'] = train_df['Morphemes'].apply(lambda x: x[0])
 train_df['Affix'] = train_df['Morphemes'].apply(lambda x: x[1] if len(x) > 1 else '')
 
--- example.py
+++ example.py
@@ -1,15 +1,16 @@
 import random
-import pandas as pd
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
 from sklearn.preprocessing import LabelEncoder
 
+# Фиксируем генераторы случайных чисел для воспроизводимости
 SEED = 1
-
 random.seed(SEED)
 np.random.seed(SEED)
 
+# Загрузка данных
 train = pd.read_csv('train_lr.csv')
 train = train.sort_values(by=['Word', 'Affix', 'Tag'])
 print(train.shape)
@@ -18,39 +19,42 @@ test = pd.read_csv('test_lr.csv')
 test = test.sort_values(by=['Word', 'Affix', 'Tag'])
 print(test.shape)
 
-# Ограничение количества слов до 1000
-train = train[:1000]
-test = test[:1000]
-
+# Объединение тренировочных и тестовых данных
 df = pd.concat([train, test], ignore_index=True)
 
+# Извлечение признаков и целевой переменной
 X = df[['Word', 'Root', 'Affix', 'PoS_root', 'PoS_word']].copy()
 y = df['Tag'].copy()
 
+# Преобразование признаков с помощью One-Hot Encoding
 X_pr = pd.get_dummies(X)
 
+# Кодирование целевой переменной
 le = LabelEncoder()
 y = le.fit_transform(y)
 
+# Разделение на тренировочный и тестовый наборы данных
 train_X = X_pr.loc[train.index, :].copy()
 train_y = y[:train.shape[0]].copy()
 
 test_X = X_pr.loc[test.index, :].copy()
 test_y = y[train.shape[0]:].copy()
 
-train_X = X_pr.loc[X_pr.index.isin(train.index), :].copy()
+train_X.shape, train_y.shape
 
+# Обучение модели
 lr = LogisticRegression()
 lr.fit(train_X, train_y)
 
+# Прогнозирование на тестовом наборе данных
 lr_predict_result = lr.predict(test_X)
-print("F1 score:", f1_score(test_y, lr_predict_result, average='micro'))
 
+print("F1 score: ", f1_score(test_y, lr_predict_result, average='micro'))
+
+# Предсказание
 SUBMISSION_FILE = 'my_submission.csv'
-
 predictions = lr.predict(test_X)
 
+# Запись результатов в CSV
 test['Tag'] = predictions
 test[['Word', 'Root', 'Affix', 'Tag']].to_csv(SUBMISSION_FILE, index=False, header=True)
-
-#Не работает 
